c9108823529039f5a320214810aca842f333a6b9
tiendungpham945-glitch <tiendungpham945@gmail.com>
2026-05-04 02:30:55 +0700

    storage.js

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,1 +1,26 @@
 
+
+
+function getUsers() {
+    return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+
+function saveUsers(users) {
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+
+function setCurrentUser(user) {
+    localStorage.setItem("currentUser", JSON.stringify(user));
+}
+
+
+function getCurrentUser() {
+    return JSON.parse(localStorage.getItem("currentUser"));
+}
+
+function logout() {
+    localStorage.removeItem("currentUser");
+    window.location.href = "login.html";
+}
